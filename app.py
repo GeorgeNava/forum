4a4604cc3779df8cbea0562d9ba28ff8b4f6513f
George Nava <georgenava@gmail.com>
2011-04-16 19:21:27 -0430

    First commit

--- app.py
+++ app.py
@@ -65,16 +65,6 @@ class request(webapp.RequestHandler):
     if file!='': name=os.path.basename(file.filename)
     return name
   
-  def startSession(self):
-    self.session = Session(self)
-
-  def endSession(self):
-    self.session.invalidate()
-    self.session = {}
-
-  def logout(self):
-    self.session.clearCookies()
-
 
 # use 'run' if you only use one route to one controller
 def run(url,main):
@@ -86,87 +76,3 @@ def control(controller):
   run_wsgi_app(webapp.WSGIApplication(controller,debug=config.debug))
 
 
-#---- SESSION --------------------------------------------------
-import random
-from google.appengine.api import memcache
-
-class Session(dict):
-  def __init__(self,handler):
-    self.handler  = handler
-    self.ipaddress= handler.request.remote_addr
-    self._timeout = 1800 # 30 min
-    self._name    = 'sid'
-    self._new     = True
-    self._invalid = False
-    dict.__init__(self)
-    name=self._name
-    
-    ok=False
-    if name in handler.request.str_cookies:
-      self._sid=handler.request.str_cookies[name]
-      data=memcache.get(self._sid)
-      if data!=None:
-        self.update(data)
-        memcache.set(self._sid,data,self._timeout)
-        self._new=False
-    else: 
-      self._sid = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in xrange(12))
-      handler.response.headers.add_header('Set-Cookie','%s=%s; path=/;'%(self._name,self._sid))
-      ok=True
-
-    if 'uid' in handler.request.str_cookies:
-      userid=handler.request.str_cookies['uid']
-      self['userid'] = userid
-      if 'tid' in handler.request.str_cookies:
-        token = handler.request.str_cookies['tid']
-        user  = models.userAutologin(userid,token)
-        if user:
-          self['username']  = user.username
-          self['islogged']  = True
-          self['lastlogin'] = utils.now()
-          self['lastip']    = handler.ipaddress
-        else:
-          self['islogged']  = False
-      ok=True
-    if ok: self.save()
-
-  def clearCookies(self):
-    headers = self.handler.response.headers
-    headers.add_header('Set-Cookie','sid=; path=/; expires=Sat, 1-Jan-2000 00:00:00 GMT;')
-    headers.add_header('Set-Cookie','tid=; path=/; expires=Sat, 1-Jan-2000 00:00:00 GMT;')
-    headers.add_header('Set-Cookie','uid=; path=/; expires=Sat, 1-Jan-2000 00:00:00 GMT;')
-    headers.add_header('Set-Cookie','usr=; path=/; expires=Sat, 1-Jan-2000 00:00:00 GMT;')
-    headers.add_header('Set-Cookie','device=; path=/; expires=Sat, 1-Jan-2000 00:00:00 GMT;')
-
-  def save(self):
-    if not self._invalid:
-      memcache.set(self._sid,self.copy(),self._timeout)
-
-  def isNew(self):
-    return self._new  #if session was created during this request
-
-  def isValid(self):
-    if 'uid' in self and not self['uid']=='': return True
-    else: return False
-
-  def Id(self):
-    return self._sid
-
-  def invalidate(self):
-    """Delete session data and cookie."""
-    self.handler.response.headers.add_header('Set-Cookie','sid=; expires=Sat, 1-Jan-2000 00:00:00 GMT;')
-    memcache.delete(self._sid)
-    self.clear()
-    self._invalid=True
-
-"""
-session={
-  'userid'    :'',
-  'username'  :'',
-  'token'     :'',
-  'islogged'  :'',
-  'lastlogin' :'',
-  'lastip'    :'',
-  'device'    :'desktop|mobile'
-}
-"""
